8d95727df35f10207189155f1c4b260a8b3d9e43
Phat Doan <phatdoan@yahoo.com>
2018-08-02 23:30:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string)lowercase.toLowerCase() === lowercase)
+  if (string).lowercase.toLowerCase() === lowercase)
   return "I can't hear you!"
 }
